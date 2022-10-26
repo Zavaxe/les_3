12d20f1ccc19d765aa673999bb59a338b4333510
Zavaxe <zavax94@gmail.com>
2022-10-26 23:09:41 +0300

    completed 2 TODO

--- 02_envelop.py
+++ 02_envelop.py
@@ -34,26 +34,39 @@ else:
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
 hole_x, hole_y = 8, 9
-brick_x, brick_y, brick_z = 11, 10, 2
-# brick_x, brick_y, brick_z = 11, 2, 10
-# brick_x, brick_y, brick_z = 10, 11, 2
-# brick_x, brick_y, brick_z = 10, 2, 11
-# brick_x, brick_y, brick_z = 2, 10, 11
-# brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+#brick_x, brick_y, brick_z = 11, 10, 2
+brick_x, brick_y, brick_z = 11, 2, 10
+brick_x, brick_y, brick_z = 10, 11, 2
+brick_x, brick_y, brick_z = 10, 2, 11
+brick_x, brick_y, brick_z = 2, 10, 11
+brick_x, brick_y, brick_z = 2, 11, 10
+brick_x, brick_y, brick_z = 3, 5, 6
+brick_x, brick_y, brick_z = 3, 6, 5
+brick_x, brick_y, brick_z = 6, 3, 5
+brick_x, brick_y, brick_z = 6, 5, 3
+brick_x, brick_y, brick_z = 5, 6, 3
+brick_x, brick_y, brick_z = 5, 3, 6
+brick_x, brick_y, brick_z = 11, 3, 6
+brick_x, brick_y, brick_z = 11, 6, 3
+brick_x, brick_y, brick_z = 6, 11, 3
+brick_x, brick_y, brick_z = 6, 3, 11
+brick_x, brick_y, brick_z = 3, 6, 11
+brick_x, brick_y, brick_z = 9, 9, 8
 # (просто раскоментировать нужную строку и проверить свой код)
 
 # TODO здесь ваш код
-brick_size = [brick_x, brick_y, brick_z]
-if hole_x > brick_size[0]
+
+if hole_x >= brick_x and hole_y >= brick_y:
+    print('Входит')
+elif hole_x >= brick_y and hole_y >= brick_x:
+    print('Входит')
+elif hole_x >= brick_x and hole_y >= brick_z:
+    print('Входит')
+elif hole_x >= brick_z and hole_y >= brick_x:
+    print('Входит')
+elif hole_x >= brick_y and hole_y >= brick_z:
+    print('Входит')
+elif hole_x >= brick_z and hole_y >= brick_y:
+    print('Входит')
+else:
+    print('Не входит')
