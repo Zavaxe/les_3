f33c5f38d1b6263933193dbfb6bab14c1a622523
Zavaxe <zavax94@gmail.com>
2022-10-26 18:27:20 +0300

    made the first TODO

--- 02_envelop.py
+++ 02_envelop.py
@@ -12,15 +12,18 @@
 envelop_x, envelop_y = 10, 7
 paper_x, paper_y = 8, 9
 # проверить для
-# paper_x, paper_y = 9, 8
-# paper_x, paper_y = 6, 8
-# paper_x, paper_y = 8, 6
-# paper_x, paper_y = 3, 4
-# paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
+#paper_x, paper_y = 9, 8
+#paper_x, paper_y = 6, 8
+#paper_x, paper_y = 8, 6
+#paper_x, paper_y = 3, 4
+#paper_x, paper_y = 11, 9
+#paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x > paper_x and envelop_y > paper_y:
+    print('Да, поместится')
+else:
+    print('Нет, не поместится')
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,8 +31,8 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
